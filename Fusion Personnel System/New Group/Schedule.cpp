a8afb578b9e569eb7dcacec09d0eb855266fb861
Joshua Pare <joshuabpare@gmail.com>
2020-05-25 20:32:15 -0400

    Added Scheduler kinda
    Added a Schedule in another Folder

---
+++ Fusion Personnel System/New Group/Schedule.cpp
@@ -0,0 +1,11 @@
+//
+//  Schedule.cpp
+//  Fusion Personnel System
+//
+//  Created by Joshua Pare on 5/25/20.
+//  Copyright © 2020 Joshua Pare. All rights reserved.
+//
+
+#include "Schedule.hpp"
+
+
